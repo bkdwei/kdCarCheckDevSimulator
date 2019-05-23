2df42c79b9b951d6029f88881b088af68d33b0bf
bkdwei <bkdwei@163.com>
2019-05-23 18:28:44 +0800

    修正响应的回复方式

--- kdCarCheckDevSimulator/monitor_thread.py
+++ kdCarCheckDevSimulator/monitor_thread.py
@@ -19,35 +19,33 @@ class monitor_thread(QThread):
         self.port = port
         self.model = model
         self.cmd_list = [c[2].upper() for c in cmd_list]
-        self.cmd_reply_type = {c[2]: c[4] for c in cmd_list}
+        self.cmd_reply_type = {c[2]: c[4] for c in cmd_list}  # 记录命令的响应方式
         print(self.cmd_reply_type)
         self.state = False
         self.reply = reply()
-        self.special_cmd_reply_list = None  # 缓存指定的类型为全部响应的命令的响应列表
-        self.special_cmd_reply_list_index = -1  # 保存全部响应的命令列表的位置
+        self.reply_index = {}  # 缓存指定的类型为全部响应的命令的响应列表的位置
 
         # 修正两个项目共用一个设备，导致无法打开串口的bug
         self.com = com
 
     def run(self):
-        try:
-            if not self.port:
-                return
+#         try:
+        if not self.port:
+            return
 
-            print("启动设备：" + self.model + ",串口：" + self.port)
-            self.show_status_signal.emit(
-                "启动设备：" + self.model + ",串口：" + self.port)
-            if not self.com:
-                self.com = serial.Serial(self.port, 9600)
-            if not self.com.isOpen():
-                self.com.open()
+        print("启动设备：" + self.model + ",串口：" + self.port)
+        self.show_status_signal.emit(
+            "启动设备：" + self.model + ",串口：" + self.port)
+        if not self.com:
+            self.com = serial.Serial(self.port, 9600)
+        if not self.com.isOpen():
+            self.com.open()
 
-            self.state = True
-            self.receiveData()
-        except Exception as e:
-            self.show_status_signal.emit(
-                "线程异常：" + self.model + ",串口：" + self.port + ",异常详情:" + str(e))
-
+        self.state = True
+        self.receiveData()
+#         except Exception as e:
+#             self.show_status_signal.emit(
+#                 "线程异常：" + self.model + ",串口：" + self.port + ",异常详情:" + str(e))
 
 #     监听数据线程
     def receiveData(self):
@@ -56,64 +54,64 @@ class monitor_thread(QThread):
                 count = self.com.in_waiting
                 if count > 0:
                     data = self.com.read(count)
-                    if data and data != b'88':
-                        receive_data = self.asciiB2HexString(
-                            data).strip().upper()
-                        print("receive:", receive_data)
+                    if data:
+                        receive_data = self.asciiB2HexString(data).strip().upper()
                         now = time.strftime(
                             '[%Y:%m:%d:%H:%M:%S]', time.localtime(time.time()))
+                        print(now + "receive:", receive_data)
                         self.show_status_signal.emit(
                             self.model + "," + now + "[接收]" + receive_data)
-                        if receive_data in self.cmd_list:
-                            #                             获取响应类型
-                            reply_type = self.cmd_reply_type[receive_data]
-                            if reply_type == 1:
-                                # 如果缓存指定的类型为全部响应的命令的响应列表为空，则缓存一下
-                                if not self.special_cmd_reply_list:
-                                    self.special_cmd_reply_list = self.reply.get_all_by_cmd_value(
-                                        receive_data)
-                                    self.special_cmd_reply_list_index = -1
+                       
+                        if not receive_data in self.cmd_list:
+                            continue
+                        
+                        # 获取响应类型
+                        if not self.cmd_reply_type :
+                            continue
+                        print("aa")
+                        reply_type = self.cmd_reply_type[receive_data]
+                        if reply_type == 1:
+                            reply_list = self.reply.get_all_by_cmd_value(receive_data)
+                            if not reply_list :
+                                continue
+                            
+                            index = 0
+                            print("ab")
+                            if receive_data in self.reply_index.keys() :
+                                index = int(self.reply_index[receive_data]) + 1
+                                # 判断是否越界
+                                if index >= len(reply_list):
+                                    index = 0
+                            self.reply_index[receive_data] = index
+                            
+                            print(index, len(reply_list))
+                            
+                            reply_item = reply_list[index]
+                            print("reply:", reply_item[1], reply_item[0])
+                            send_msg = self.hexStringB2Hex(reply_item[0])
+                            self.com.write(send_msg)
+                            now = time.strftime(
+                                '[%Y:%m:%d:%H:%M:%S]', time.localtime(time.time()))
+                            self.show_status_signal.emit(
+                                self.model + "," + now + "[发送]" + reply_item[0] + "  -  " + reply_item[1])
+                            time.sleep(2)
+                        else:
+                            reply_list = self.reply.get_all_by_cmd_value(receive_data)
 
-                                # 发送下一个全部响应命令列表的下一个响应
-                                if self.special_cmd_reply_list:
-                                    self.special_cmd_reply_list_index += 1
-                                    print(self.special_cmd_reply_list_index, len(
-                                        self.special_cmd_reply_list))
-                                    # 判断是否越界
-                                    if self.special_cmd_reply_list_index >= len(self.special_cmd_reply_list):
-                                        self.special_cmd_reply_list_index = 0
-                                    reply_item = self.special_cmd_reply_list[self.special_cmd_reply_list_index]
-                                    print(
-                                        "reply:", reply_item[1], reply_item[0])
-                                    send_msg = self.hexStringB2Hex(
-                                        reply_item[0])
-                                    self.com.write(send_msg)
-                                    now = time.strftime(
-                                        '[%Y:%m:%d:%H:%M:%S]', time.localtime(time.time()))
-                                    self.show_status_signal.emit(
-                                        self.model + "," + now + "[发送]" + reply_item[0] + "  -  " + reply_item[1])
-                                    time.sleep(3)
-                            else:
-                                # 如果缓存指定的类型为全部响应的命令的响应列表为空，则缓存一下
-                                if not self.special_cmd_reply_list:
-                                    self.special_cmd_reply_list = self.reply.get_all_by_cmd_value(
-                                        receive_data)
-
-                                index = random.randint(-1,
-                                                       len(self.special_cmd_reply_list) - 1)
-                                print("index:" + str(index))
-                                print(
-                                    "reply:", self.special_cmd_reply_list[index][0])
-                                send_msg = self.hexStringB2Hex(
-                                    self.special_cmd_reply_list[index][0])
-                                self.com.write(send_msg)
-                                self.show_status_signal.emit(
-                                    self.model + "," + now + "[发送]" + self.special_cmd_reply_list[index][0] + "  -  " + self.special_cmd_reply_list[index][1])
-#                     self.com.close()
-#                     break
+                            if len(reply_list) == 0:
+                                continue
+                            index = random.randint(-1,
+                                                   len(reply_list) - 1)
+                            print("index:" + str(index))
+                            print(
+                                "reply:", reply_list[index][0])
+                            send_msg = self.hexStringB2Hex(
+                                reply_list[index][0])
+                            self.com.write(send_msg)
+                            self.show_status_signal.emit(
+                                self.model + "," + now + "[发送]" + reply_list[index][0] + "  -  " + reply_list[index][1])
         except KeyboardInterrupt:
-            if self.com != None:
-                self.com.close()
+            pass
 
 #     字节数组转16进制字符串
     def asciiB2HexString(self, strB):
